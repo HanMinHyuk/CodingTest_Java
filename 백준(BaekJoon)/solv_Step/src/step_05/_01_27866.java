a73980a7111fc207dc4a8c4522340a3a33c8055a
IllwonDongJobs <alsgurgks63@naver.com>
2024-02-06 20:59:15 +0900

    BaekJoon 단계별 풀어보기
    1문제 완료

---
+++ 백준(BaekJoon)/solv_Step/src/step_05/_01_27866.java
@@ -0,0 +1,13 @@
+package step_05;
+import java.util.*;
+
+public class _01_27866 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String S = sc.nextLine();
+        int i = sc.nextInt();
+
+        System.out.println(S.charAt(i-1));
+    }
+}
