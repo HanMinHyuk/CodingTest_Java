a4b04577f6182c3d751453a1b4f74b99927ba3e1
IllwonDongJobs <alsgurgks63@naver.com>
2024-02-08 23:00:23 +0900

    프로그래머스 Lv.0
    1문제 완료

---
+++ 프로그래머스 (Programmers)/Lv.0/_02_2743.java
@@ -0,0 +1,12 @@
+package step_05;
+import java.util.*;
+
+public class _02_2743 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String word = sc.nextLine();
+
+        System.out.println(word.length());
+    }
+}
